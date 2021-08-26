ca07e87ffd02600f2ae19aa4915eb0bd3d10ff24
Alexorcev <Alexorcev@mail.ru>
2021-08-26 19:36:13 +0300

    create: form validation

--- js/main.js
+++ js/main.js
@@ -54,5 +54,28 @@ $(document).ready(function () {
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
   } 
-
+// Обработка форм
+  $('.form').each(function() {
+    $(this).validate({
+    errorClass: "invalid",
+    messages: {
+    name: {
+      required: "Please specify your name",
+    },
+    email: {
+      required: "We need your email address to contact you",
+      email: "Your email address must be in the format of name@domain.com"
+    },
+    phone: {
+      required: "Indicate your phone number",
+      
+    },
+  }
+  });
+  })
+  //Маска для номера телефона
+  $(document).ready(function(){
+  $('.phone').mask('+7 (999) 999-99-99');
+  
+});
 });
